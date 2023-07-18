f9210c30dbf2d566b37c2e302d29f422b18b96b2
CHISOM EDOKA <edokachisom48@gmail.com>
2023-07-18 13:28:00 +0100

    created a post route that checkes the legnth of the movie genre

--- routes/genres.js
+++ routes/genres.js
@@ -1,11 +1,36 @@
 const express = require("express");
-//import { Router } from "express";
 const router = express.Router(); // This is a mini express application
+const Joi = require("joi");
 
-const genres = ["Action", "Comedy", "Drama"];
+// This is a mock database
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Horror" },
+  { id: 3, name: "Romance" },
+];
 
+// This is a route handler
 router.get("/", (req, res) => {
   res.send(genres);
 });
+
+router.post("/", (req, res) => {
+  const { value, error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+// function to validate courses added to the database
+const validateCourse = (course) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(), // name is required and must be at least 3 characters
+  });
+  return schema.validate(course);
+};
 
 module.exports = router;
