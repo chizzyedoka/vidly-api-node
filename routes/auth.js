f91c319a8198a77d7c2f0e76682a804c9934d4b4
CHISOM EDOKA <edokachisom48@gmail.com>
2023-08-03 12:07:10 +0100

    Generated jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const bcrypt = require("bcrypt");
 const { User } = require("../models/users");
@@ -13,7 +14,8 @@ router.post("/", async (req, res) => {
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password");
-  res.send(true);
+  const token = jwt.sign({ _id: user._id }, "jwtPrivateKey");
+  res.send(token);
 });
 
 function validate(req) {
